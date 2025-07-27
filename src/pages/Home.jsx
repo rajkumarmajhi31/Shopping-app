46ad2366f8aa815a673426bc8dbcf355ab97007d
Rajkumar Majhi <118106939+rajkumarmajhi31@users.noreply.github.com>
2025-07-27 21:26:41 +0530

    updated css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
   return (
     <div>
       {loading ? (
-        <Spinner />
+         <div className= " pt-6 flex-grow flex items-center justify-center " > <Spinner /> </div>
       ) : posts.length > 0 ? (
         <div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
           {posts.map((post) => (
